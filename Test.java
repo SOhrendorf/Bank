397102f84af0a8df23b7227fe64810d2c365030f
SOhrendorf <S.Ohrendorf@web.de>
2020-11-26 08:31:58 +0100

    Test versuche

--- Test.java
+++ Test.java
@@ -2,15 +2,36 @@
 
 public class Test
 {
-
+    Konto konto1;
+    Konto konto2;
+    Konto konto3;
+    Konto konto4;
 
     public Test()
     {
-
+        konto1 = new Konto(1, 1);
+        konto2 = new Konto(2, 2);
+        konto3 = new Konto(3, 3);
+        konto4 = new Konto(4, 4);
     }
 
-    public void beispielMethode(int y)
+    public void testEinzahlen()
     {
-
+        konto1.einzahlen(1000);
+        konto2.einzahlen(2000);
+        konto3.einzahlen(3000);
+        konto4.einzahlen(4000);
+    }
+    
+    public void testAuszahlen(){
+        konto1.auszahlen(500,1); //richtig
+        konto2.auszahlen(500, 100); //falscher Pin
+        konto3.auszahlen(10000000, 3); // zu hoher Betrag
+    }
+    
+    public void testUeberweisen(){
+        konto1.ueberweisen(500, 1, konto2); //richtig
+        konto1.ueberweisen(500, 10, konto2); //falscher Pin
+        konto1.ueberweisen(1000000, 1, konto2); //zu hoher Betrag
     }
 }
